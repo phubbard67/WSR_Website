ad032da137a480ed94ad7f0164e777e10272f6bd
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-01 22:48:01 -0800

    Testing

--- website/scripts/myscripts.js
+++ website/scripts/myscripts.js
@@ -50,7 +50,25 @@ document.addEventListener('DOMContentLoaded', function () {
         currentYear.textContent = thisYear;
       }
 
-      getWeatherInfo();
+      const request = require('request');
+      var API_KEY = 'cee21d7bcdb1e3973d14849c3cda02b4';
+    
+      const apiKey = 'cee21d7bcdb1e3973d14849c3cda02b4'; 
+      const city = 'Portland'; 
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`Network response was not ok: ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(`Cloud Percentage in ${city}: ${data.clouds.all}%`);
+        })
+        .catch(error => {
+          console.error('There was a problem with your fetch operation:', error);
+        });
 });
 //---End Event Handlers 
 

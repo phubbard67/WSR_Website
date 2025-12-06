4637bab5631ddd3d47fc86efcc2d8059306eb5c1
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-05 22:55:30 -0800

    Fixed an issue with the weather api not working with node, but still figuring stuff out.

--- website/scripts/weather.js
+++ website/scripts/weather.js
@@ -8,20 +8,18 @@ const apiKey = 'cee21d7bcdb1e3973d14849c3cda02b4';
 const city = 'Portland'; 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const CurrentWeatherData = request({url: url, json: true}, function(error, response){
-        if(error){
-            console.log("OH NO!!!! FART! FART! FART!");
-        }
-        else{
-            WeatherMain = response.body.main;
-            WeatherOverview = response.body.weather[0];
-            CurrentSpankButtArea = "place where the band lives (Portland):\n   ";
-    
-    
-            console.log(`The current weather description for ${CurrentSpankButtArea}${WeatherOverview.description}\n`);
-            console.log(`The Temperature of the ${CurrentSpankButtArea}${WeatherMain.temp}C° \ ${CToFHeight(WeatherMain.temp)}F°\n`);
-            console.log(`What it feels like in the ${CurrentSpankButtArea}${WeatherMain.feels_like}C° \ ${CToFHeight(WeatherMain.feels_like)}F°\n`);
-        }
-    });
+const CurrentWeatherData = request({url: url, json: true}, function(error, response){
+    if(error){
+        console.log("OH NO!!!! FART! FART! FART!");
+    }
+    else{
+        WeatherMain = response.body.main;
+        WeatherOverview = response.body.weather[0];
+        CurrentSpankButtArea = "place where the band lives (Portland):\n   ";
+
+
+        console.log(`The current weather description for ${CurrentSpankButtArea}${WeatherOverview.description}\n`);
+        console.log(`The Temperature of the ${CurrentSpankButtArea}${WeatherMain.temp}C° \ ${CToFHeight(WeatherMain.temp)}F°\n`);
+        console.log(`What it feels like in the ${CurrentSpankButtArea}${WeatherMain.feels_like}C° \ ${CToFHeight(WeatherMain.feels_like)}F°\n`);
+    }
 });

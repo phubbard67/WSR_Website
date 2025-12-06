02816d171d7af0c5815ef329abfb4677b8498b70
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-05 22:47:47 -0800

    Starting the random number generator: I finished my basic weather app, so now I'm going to use the cloud percentage as a random number to change the website by updating the greyscale on the photos based on the percentage of cloud cover in Portland, OR.

--- website/scripts/myscripts.js
+++ website/scripts/myscripts.js
@@ -2,25 +2,6 @@
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-
-function getWeatherInfo() {
-  const apiKey = 'cee21d7bcdb1e3973d14849c3cda02b4'; 
-  const city = 'Portland'; 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(`Cloud Percentage in ${city}: ${data.clouds.all}%`);
-    })
-    .catch(error => {
-      console.error('There was a problem with your fetch operation:', error);
-    });
-}
 //---End Helper Functions
 
 //--------------------------------------------------------Event Handlers
---
+++ website/scripts/weather.js
@@ -0,0 +1,27 @@
+const request = require('request');
+var API_KEY = 'cee21d7bcdb1e3973d14849c3cda02b4';
+const CToFHeight = function(Cels)
+{
+    return Cels * (9/5) + 32;
+}
+const apiKey = 'cee21d7bcdb1e3973d14849c3cda02b4'; 
+const city = 'Portland'; 
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const CurrentWeatherData = request({url: url, json: true}, function(error, response){
+        if(error){
+            console.log("OH NO!!!! FART! FART! FART!");
+        }
+        else{
+            WeatherMain = response.body.main;
+            WeatherOverview = response.body.weather[0];
+            CurrentSpankButtArea = "place where the band lives (Portland):\n   ";
+    
+    
+            console.log(`The current weather description for ${CurrentSpankButtArea}${WeatherOverview.description}\n`);
+            console.log(`The Temperature of the ${CurrentSpankButtArea}${WeatherMain.temp}C° \ ${CToFHeight(WeatherMain.temp)}F°\n`);
+            console.log(`What it feels like in the ${CurrentSpankButtArea}${WeatherMain.feels_like}C° \ ${CToFHeight(WeatherMain.feels_like)}F°\n`);
+        }
+    });
+});

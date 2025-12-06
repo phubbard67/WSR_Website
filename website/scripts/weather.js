86fb70b80ce641c5a0b6f924d855db366693eb4a
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-06 01:49:14 -0800

    I DID IT! Now, my website uses a truely random value to update the images. It uses the cloud cover percentage to change the grayscale of the images. More clouds, more gray, and in a truely random way. ;)

--- website/scripts/weather.js
+++ website/scripts/weather.js
@@ -3,7 +3,7 @@ const CToFHeight = function(Cels)
     return Cels * (9/5) + 32;
 } 
 
-async function getData() {
+async function UseRandomPowerToUpdateImages() {
     const apiKey = 'cee21d7bcdb1e3973d14849c3cda02b4'; 
     const city = 'Portland'; 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -19,13 +19,13 @@ async function getData() {
       console.log(result.clouds.all)
 
       newGrayScale = 0;
-      if(result.clouds.all > 40)
+      if(result.clouds.all > 10)
       {
         newGrayScale = result.clouds.all;
       }
       else
       {
-        newGrayScale = 40;
+        newGrayScale = 10;
       }
 
       images = document.getElementsByClassName("image");
@@ -40,3 +40,11 @@ async function getData() {
       console.error(error.message);
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    //This function uses a "real" random number produced by the 
+    //  weather to update the grayscale of the images. when it is cloudy, 
+    //  the images are more gray, and less clouds, less gray, but it will change
+    //  randomly, truley randomly. no more Srand for this guy! R-Rand has begun! :)
+    UseRandomPowerToUpdateImages();
+});

ce18635d96e96618fc944c860733092d2a0cd56d
paulHubbard.prsn <phubbard67@gmail.com>
2025-12-06 22:50:58 -0800

    Using NOAA free data, as it has no data limit!!

--- website/scripts/weather.js
+++ website/scripts/weather.js
@@ -3,34 +3,50 @@ const CToFHeight = function(Cels)
     return Cels * (9/5) + 32;
 } 
 
+const options = {
+  latitude: 45.5233729,
+  longitude: -122.6885142,
+  PortlandOrWeatherOptionsUrl: 'https://api.weather.gov/points/45.5233729,-122.6885142',
+  PortlandforcastUrl: 'https://api.weather.gov/gridpoints/PQR/112,104/forecast',
+  PortlandForecast: 'https://api.weather.gov/gridpoints/PQR/112,104/forecast',
+  PortlandForecastHourly: 'https://api.weather.gov/gridpoints/PQR/112,104/forecast/hourly',
+  PortlandForecastGridData: 'https://api.weather.gov/gridpoints/PQR/112,104',
+  PortlandObservationStations: 'https://api.weather.gov/gridpoints/PQR/112,104/stations',
+}
+
 async function UseRandomPowerToUpdateImages() {
-    const apiKey = 'cee21d7bcdb1e3973d14849c3cda02b4'; 
-    const city = 'Portland'; 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const grayScaleMin = 10;
 
     try {
-      const response = await fetch(url);
+      const response = await fetch(options.PortlandForecastGridData, {
+        method: "POST", 
+        headers: {
+          "User-Agent" : "(https://walkingstalkingrobots.com, walkingstalkingrobots@gmail.com)"
+        }
+      });
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
       
       const result = await response.json();
+      const CurrentCloudPercentInPortland = result.properties.skyCover.values[0].value;
+
+      console.log(CurrentCloudPercentInPortland);
 
       newGrayScale = 0;
-      if(result.clouds.all == 100)
+      if(CurrentCloudPercentInPortland == 100)
       {
-        newGrayScale = result.clouds.all;
+        newGrayScale = CurrentCloudPercentInPortland;
         console.log("Wow.\nAnother day of 100% cloud coverage in Portland.\n");
       }
-      else if(result.clouds.all == 0)
+      else if(CurrentCloudPercentInPortland == 0)
       {
-        newGrayScale = result.clouds.all;
+        newGrayScale = CurrentCloudPercentInPortland;
         console.log("WHAT THE FUCK?!?!??!?!?!?!\n    ZERO CLOUDS IN PORTLAND TODAY?!?!?!\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!PARTY!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
       }
-      else if(result.clouds.all > grayScaleMin)
+      else if(CurrentCloudPercentInPortland > grayScaleMin)
       {
-        newGrayScale = result.clouds.all;
+        newGrayScale = CurrentCloudPercentInPortland;
       }
       else
       {
